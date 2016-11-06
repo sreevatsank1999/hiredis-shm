99108ec0c9751d3d1c819b1a6310f66302db6ec0
Edgars <edgars.irmejs@gmail.com>
2016-11-06 18:40:09 +0200

    GCC optimized away shared memory accesses. Fixed that.

--- shm.c
+++ shm.c
@@ -51,13 +51,13 @@
 /*TODO: Use reason when choosing the size!*/
 #define SHARED_MEMORY_BUF_SIZE (1024-8-8-8) /* ...the last should be 1, for buffer always leaves a byte unused. i just prefer sizes divide by 8. */
 
-typedef struct sharedMemoryBuffer {
+typedef volatile struct sharedMemoryBuffer {
     char buf[SHARED_MEMORY_BUF_SIZE];
     size_t read_idx;
     size_t write_idx;
 } sharedMemoryBuffer;
 
-typedef struct sharedMemory {
+typedef volatile struct sharedMemory {
     sharedMemoryBuffer to_server;
     sharedMemoryBuffer to_client;
 } sharedMemory;
@@ -146,6 +146,23 @@ void sharedMemoryAfterConnect(redisContext *c) {
     freeReplyObject(reply);
 }
 
+/* Return the UNIX time in microseconds */
+static long long ustime(void) {
+    struct timeval tv;
+    long long ust;
+
+    gettimeofday(&tv, NULL);
+    ust = ((long long)tv.tv_sec)*1000000;
+    ust += tv.tv_usec;
+    return ust;
+}
+
+/*inline void
+clflush(volatile void *p)
+{
+    asm volatile ("clflush (%0)" :: "r"(p));
+}*/
+
 static size_t bufFreeSpace(sharedMemoryBuffer *target) {
     size_t read_idx = target->read_idx;
     ssize_t free = (ssize_t)read_idx - target->write_idx - 1;
@@ -156,25 +173,29 @@ static size_t bufFreeSpace(sharedMemoryBuffer *target) {
 }
 
 static void bufWrite(sharedMemoryBuffer *target, char *buf, size_t btw) {
-    X("bufWrite(read_ptr=%d write_ptr=%d btw=%d\n", target->read_idx, target->write_idx, btw);
+    X("%lld bufWrite(read_ptr=%d write_ptr=%d btw=%d\n", ustime(), target->read_idx, target->write_idx, btw);
     size_t write_idx = target->write_idx;
     /* TODO: Need an atomic read and write for 32bit systems. See if atomicvar.h in the redis/src can help. */
     __sync_synchronize();
+    char* target_buf = (char*)target->buf; /* casting away volatile because buf can't change in the range being used. */
     if (write_idx >= target->read_idx) {
         size_t bytes_to_eob = sizeof(target->buf) - write_idx;
         if (bytes_to_eob <= btw) {
             /* Write needs to rotate target->write_idx to the buffer beginning */
-            memcpy(target->buf + write_idx, buf, bytes_to_eob);
+            memcpy(target_buf + write_idx, buf, bytes_to_eob);
             write_idx = 0;
             btw -= bytes_to_eob;
             buf += bytes_to_eob;
         }
     }
-    memcpy(target->buf + write_idx, buf, btw);
+    memcpy(target_buf + write_idx, buf, btw);
     write_idx += btw;
     __sync_synchronize();
     target->write_idx = write_idx;
-    X("bufWrite fin (read_ptr=%d write_ptr=%d btw=%d\n", target->read_idx, target->write_idx, btw);
+    /* Need to push out of L1 cache, for other CPUs to see the update now. */
+//    asm volatile ("mfence" ::: "memory");
+//    clflush(&target->write_idx);
+    X("%lld bufWrite fin (read_ptr=%d write_ptr=%d btw=%d\n", ustime(), target->read_idx, target->write_idx, btw);
 }
 
 /* TODO: Less duplication, maybe? */
@@ -189,35 +210,28 @@ static size_t bufUsedSpace(sharedMemoryBuffer *source) {
 
 /* TODO: Less duplication, maybe? */
 static void bufRead(sharedMemoryBuffer *source, char *buf, size_t btr) {
-    X("bufRead(read_ptr=%d write_ptr=%d btr=%d\n", source->read_idx, source->write_idx, btr);
+    X("%lld bufRead(read_ptr=%d write_ptr=%d btr=%d\n", ustime(), source->read_idx, source->write_idx, btr);
     size_t read_idx = source->read_idx;
     __sync_synchronize();
+    char* source_buf = (char*)source->buf; /* casting away volatile because buf can't change in the range being used. */
     if (read_idx > source->write_idx) {
         size_t bytes_to_eob = sizeof(source->buf) - read_idx;
         if (bytes_to_eob <= btr) {
             /* Read needs to rotate source->read_idx to the buffer beginning */
-            memcpy(buf, source->buf + read_idx, bytes_to_eob);
+            memcpy(buf, source_buf + read_idx, bytes_to_eob);
             read_idx = 0;
             btr -= bytes_to_eob;
             buf += bytes_to_eob;
         }
     }
-    memcpy(buf, source->buf + read_idx, btr);
+    memcpy(buf, source_buf + read_idx, btr);
     read_idx += btr;
     __sync_synchronize();
     source->read_idx = read_idx;
-    X("bufRead fin (read_ptr=%d write_ptr=%d btr=%d\n", source->read_idx, source->write_idx, btr);
-}
-
-/* Return the UNIX time in microseconds */
-static long long ustime(void) {
-    struct timeval tv;
-    long long ust;
-
-    gettimeofday(&tv, NULL);
-    ust = ((long long)tv.tv_sec)*1000000;
-    ust += tv.tv_usec;
-    return ust;
+    /* Need to push out of L1 cache, for other CPUs to see the update now. */
+//    asm volatile ("mfence" ::: "memory");
+//    clflush(&source->read_idx);
+    X("%lld bufRead fin (read_ptr=%d write_ptr=%d btr=%d\n", ustime(), source->read_idx, source->write_idx, btr);
 }
 
 int sharedMemoryWrite(redisContext *c, char *buf, size_t btw) {
